ccd7e1081cb900edff54f541e05efa78d222642e
João Santos <joaosantossp2020@gmail.com>
2021-01-13 00:48:17 -0300

    estilofinalizado

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@
 var $counting = (document.getElementById("ul").getElementsByTagName("li").length)
 var $ul = document.getElementById('ul')
 var $input = document.getElementById('input')
+var $dateTitle = document.getElementById('dateTitle')
+var $hoursTitle = document.getElementById('hoursTitle')
+
+window.onload = time()
+
+//Função de criação do relógio
+function time(){
+    var $date = new Date()
+    var $day = $date.getDate()
+    var $month = $date.getMonth() + 1
+    var $year = $date.getFullYear()
+
+    const $event = new Date($year, $month, $day)
+    const $optionsMonth = {month: 'long'}
+    const $optionsWeek = {weekday: 'long'}
+
+    var $monthNow = $event.toLocaleDateString(undefined, $optionsMonth)
+    var $weekDayNow = $event.toLocaleDateString(undefined, $optionsWeek)
+
+    document.getElementById('dayTitle').textContent = $day
+    document.getElementById('monthTitle').textContent = $monthNow
+    document.getElementById('yearTitle').textContent = $year
+    document.getElementById('weekDay').textContent = $weekDayNow
+
+
+}
 
 //Chama a função addItemTodo() ao pressionar a tecla enter
 $input.addEventListener('keyup', (event) => {
@@ -22,9 +48,11 @@ function addItemTodo(){
     $counting += 1
 
     //Valor máximo de todos (10) Previne a criação de mais
-    if ($counting>10){
+    if ($counting>9){
         $counting = $counting - 1
+        $input.style.backgroundColor = '#a770ef'
         $input.disabled = true
+        $input.value = ''
         return
     }
 
@@ -46,6 +74,7 @@ function addItemTodo(){
         if ($alertClearOne == true){
             $li.style.display = 'none'
             $counting -= 1
+            $input.style.backgroundColor = 'white'
             $input.disabled = false
         }
     }
@@ -59,6 +88,7 @@ function clearTodoList(){
         $counting = 0
         $ul.innerHTML = ''
         $input.value = ''
+        $input.style.backgroundColor = 'white'
         $input.disabled = false
     }
 }
